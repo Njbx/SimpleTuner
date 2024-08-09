1612f87f6a252acbb493134153147a6846f2a256
Njbx <80281506+Njbx@users.noreply.github.com>
2024-08-10 01:38:39 +0200

    Update train.py

--- train.py
+++ train.py
@@ -2790,7 +2790,7 @@ if __name__ == "__main__":
     try:
         import multiprocessing
 
-        multiprocessing.set_start_method("fork")
+        multiprocessing.set_start_method("spawn")
     except Exception as e:
         logger.error(
             "Failed to set the multiprocessing start method to 'fork'. Unexpected behaviour such as high memory overhead or poor performance may result."
